f094e18e2b7f6eda05ebc7bd82e03119fef2457a
s325909 <jaso_cyril@hotmail.com>
2022-02-11 15:51:01 +0100

    Feat: bank loan check requirements with alerts or add amount to balance

--- komputer-store/script.js
+++ komputer-store/script.js
@@ -9,6 +9,7 @@ const workLaborBtnElement = document.querySelector('#workLaborBtn')
 const laptopSelectElement = document.querySelector('#laptopSelect')
 
 const balanceNOK = " Kr."
+let hasLoan = false;
 
 workLaborBtnElement.addEventListener('click', e => {
     const prevBalance = Number.parseInt(workBalanceElement.innerHTML);
@@ -22,3 +23,22 @@ workBankBtnElement.addEventListener('click', e => {
     workBalanceElement.innerHTML = 0 + balanceNOK;
 });
 
+bankLoanBtnElement.addEventListener('click', e => {
+    if (hasLoan) {
+        alert("Reject! You may not have two loans at once. The initial loan should be paid back in full.")
+        return;
+    }
+
+    const loanAmount = Number(window.prompt("Enter an amount: ", ""));
+    const bankBalance = Number.parseInt(bankBalanceElement.innerHTML);
+
+    if (loanAmount > bankBalance * 2) {
+        alert("Rejected! You cannot get a loan more than double of your bank balance!")
+        return;
+    }
+
+    // const eligible = checkLoanRequirements(loanAmount, bankBalance)
+
+    bankBalanceElement.innerHTML = bankBalance + loanAmount + balanceNOK;
+    hasLoan = true;
+})
